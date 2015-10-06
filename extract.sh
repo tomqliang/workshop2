a07edf6dbf57c844c71cb4f41db8b4e4610593df
tomqliang <q.liang@163.com>
2015-10-06 15:37:33 -0600

    Created first draft of our amazingly powerful script

--- extract.sh
+++ extract.sh
@@ -1,5 +1,6 @@
 #This is a shell script including nothing
-Qing Liang October 6, 2015
+#Qing Liang October 6, 2015
 
 ## Just doing the exercise as instructed...
 
+cut -f 2 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv
