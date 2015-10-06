0e3a8ebbdd4dbc18feffe13fb235221c2743d502
prat9 <psbgupta@ucalgary.ca>
2015-10-06 15:58:53 -0600

    updated the language

--- extract.sh
+++ extract.sh
@@ -1,6 +1,6 @@
 #This is a shell script including nothing
 #Qing Liang October 6, 2015
 
-## This script will extract specific data from user specified columns...
+## This script will extract specific data from user specified columns
 
 cut -f $1 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv
